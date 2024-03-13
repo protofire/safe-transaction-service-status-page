9a73784eb5a083d591d601796f70e91aed228201
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-03-13 18:34:46 +0700

    feat: add Sei config services

--- src/components/config-service-url-selector/ConfigServiceUrlSelector.tsx
+++ src/components/config-service-url-selector/ConfigServiceUrlSelector.tsx
@@ -118,6 +118,14 @@ const defaultOptions = [
     value: "https://config.staging.safe.rootstock.io",
   },
   {
+    label: "Sei PRD",
+    value: "https://config.sei-safe.protofire.io",
+  },
+  {
+    label: "Sei STG",
+    value: "https://config.staging.sei-safe.protofire.io",
+  },
+  {
     label: "ZetaChain PRD",
     value: "https://config.safe.zetachain.com",
   },
