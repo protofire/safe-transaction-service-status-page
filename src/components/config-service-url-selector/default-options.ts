fced6533233d13b52ba039753d13f77e91ae7267
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-04-25 14:47:13 +0800

    feat: add new deployments

---
+++ src/components/config-service-url-selector/default-options.ts
@@ -0,0 +1,202 @@
+export const defaultOptions = [
+  {
+    label: "Astar PRD",
+    value: "https://config.safe.astar.network",
+  },
+  {
+    label: "Astar STG",
+    value: "https://config.staging-safe.astar.network",
+  },
+  {
+    label: "Berachain PRD",
+    value: "https://config.safe.berachain.com",
+  },
+  {
+    label: "Berachain STG",
+    value: "https://config.staging.safe.berachain.com",
+  },
+  {
+    label: "Blast",
+    value: "https://config.blast-safe.io",
+  },
+  {
+    label: "BOB PRD",
+    value: "https://config.safe.gobob.xyz",
+  },
+  {
+    label: "BOB STG",
+    value: "https://config.staging.safe.gobob.xyz",
+  },
+  {
+    label: "CrossFi PRD",
+    value: "https://config.safe.crossfi.org",
+  },
+  {
+    label: "CrossFi STG",
+    value: "https://config.staging.safe.crossfi.org",
+  },
+  {
+    label: "Evmos PRD",
+    value: "https://config.safe.evmos.org",
+  },
+  {
+    label: "Evmos STG",
+    value: "https://config.safe.evmos.dev",
+  },
+  {
+    label: "Gelato | Showcase",
+    value: "https://config.gelato-safe.protofire.io",
+  },
+  {
+    label: "Gelato | Reya PRD",
+    value: "https://config.safe.reya.network",
+  },
+  {
+    label: "Gelato | Reya STG",
+    value: "https://config.staging.safe.reya.network",
+  },
+  {
+    label: "Gelato | Tangible PRD",
+    value: "https://config.safe.re.al",
+  },
+  {
+    label: "Gelato | Tangible STG",
+    value: "https://config.staging.safe.re.al",
+  },
+  {
+    label: "Harmony PRD",
+    value: "https://config.multisig.harmony.one",
+  },
+  {
+    label: "Harmony STG",
+    value: "https://config.staging-safe.harmony.one",
+  },
+  {
+    label: "Holesky PRD",
+    value: "https://config.holesky-safe.protofire.io",
+  },
+  {
+    label: "Holesky STG",
+    value: "https://config.stg.holesky-safe.protofire.io",
+  },
+  {
+    label: "Immutable PRD",
+    value: "https://config.safe.immutable.com",
+  },
+  {
+    label: "Immutable STG",
+    value: "https://config.safe.staging.immutable.com",
+  },
+  {
+    label: "Kroma PRD",
+    value: "https://config.safe.kroma.network",
+  },
+  {
+    label: "Kroma STG",
+    value: "https://config.staging.safe.kroma.network",
+  },
+  {
+    label: "Linea PRD",
+    value: "https://config.safe.linea.build",
+  },
+  {
+    label: "Linea STG",
+    value: "https://config.staging.safe.linea.build",
+  },
+  {
+    label: "Mantle PRD",
+    value: "https://config.multisig.mantle.xyz",
+  },
+  {
+    label: "Mantle STG",
+    value: "https://config.staging.multisig.mantle.xyz",
+  },
+  {
+    label: "Moonbeam PRD",
+    value: "https://config.multisig.moonbeam.network",
+  },
+  {
+    label: "Moonbeam STG",
+    value: "https://config.staging.multisig.moonbeam.network",
+  },
+  {
+    label: "NeonEVM PRD",
+    value: "https://config.safe.neonevm.org",
+  },
+  {
+    label: "NeonEVM STG",
+    value: "https://config.staging.safe.neonevm.org",
+  },
+  {
+    label: "Oasis PRD",
+    value: "https://config.safe.oasis.io",
+  },
+  {
+    label: "Oasis STG",
+    value: "https://config.safe.stg.oasis.io",
+  },
+  {
+    label: "Rootstock PRD",
+    value: "https://config.safe.rootstock.io",
+  },
+  {
+    label: "Rootstock STG",
+    value: "https://config.staging.safe.rootstock.io",
+  },
+  {
+    label: "Scroll PRD",
+    value: "https://config.safe.scroll.xyz",
+  },
+  {
+    label: "Scroll STG",
+    value: "https://config.staging.safe.scroll.xyz",
+  },
+  {
+    label: "Sei PRD",
+    value: "https://config.sei-safe.protofire.io",
+  },
+  {
+    label: "Sei STG",
+    value: "https://config.staging.sei-safe.protofire.io",
+  },
+  {
+    label: "Superchain PRD",
+    value: "https://config.safe.optimism.io",
+  },
+  {
+    label: "Superchain STG",
+    value: "https://config.staging.safe.optimism.io",
+  },
+  {
+    label: "Taiko PRD",
+    value: "https://config.safe.taiko.xyz",
+  },
+  {
+    label: "Taiko STG",
+    value: "https://config.staging.safe.taiko.xyz",
+  },
+  {
+    label: "Wemix PRD",
+    value: "https://config.safe.wemix.com",
+  },
+  {
+    label: "Wemix STG",
+    value: "https://config.staging.safe.wemix.com",
+  },
+  {
+    label: "ZetaChain PRD",
+    value: "https://config.safe.zetachain.com",
+  },
+  {
+    label: "ZetaChain STG",
+    value: "https://config.staging.safe.zetachain.com",
+  },
+  {
+    label: "Zilliqa EVM PRD",
+    value: "https://config.safe.zilliqa.com",
+  },
+  {
+    label: "Zilliqa EVM STG",
+    value: "https://config.staging.safe.zilliqa.com",
+  },
+];
