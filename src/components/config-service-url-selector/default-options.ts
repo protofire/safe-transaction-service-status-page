5a099348def45a91c4ab076d71a558cf770095b7
Denis Smolonski <denissmolonski@gmail.com>
2025-03-21 10:42:59 +0100

    feat: add nibiru

---
+++ src/components/config-service-url-selector/default-options.ts
@@ -0,0 +1,399 @@
+export const defaultOptionsPRD = [
+  {
+    label: "Astar PRD",
+    value: "https://config.safe.astar.network",
+  },
+  {
+    label: "Berachain PRD",
+    value: "https://config.safe.berachain.com",
+  },
+  {
+    label: "Blast",
+    value: "https://config.blast-safe.io",
+  },
+  {
+    label: "Bob PRD",
+    value: "https://config.safe.gobob.xyz",
+  },
+  {
+    label: "BOBA PRD",
+    value: "https://config.safe.boba.network",
+  },
+  {
+    label: "CrossFi PRD",
+    value: "https://config.safe.crossfi.org",
+  },
+  {
+    label: "Cronos PRD",
+    value: "https://config.cronos-safe.org",
+  },
+  {
+    label: "Evmos PRD",
+    value: "https://config.safe.evmos.org",
+  },
+  {
+    label: "Gelato | Reya PRD",
+    value: "https://config.safe.reya.network",
+  },
+  {
+    label: "Gelato | Tangible PRD",
+    value: "https://config.safe.re.al",
+  },
+  {
+    label: "Harmony PRD",
+    value: "https://config.multisig.harmony.one",
+  },
+  {
+    label: "Holesky PRD",
+    value: "https://config.holesky-safe.protofire.io",
+  },
+  {
+    label: "Immutable PRD",
+    value: "https://config.safe.immutable.com",
+  },
+  {
+    label: "Flow PRD",
+    value: "https://config.safe.flow.com",
+  },
+  {
+    label: "IoTeX PRD",
+    value: "https://config.safe.iotex.io",
+  },
+  {
+    label: "Kroma PRD",
+    value: "https://config.safe.kroma.network",
+  },
+  {
+    label: "Linea PRD",
+    value: "https://config.safe.linea.build",
+  },
+  {
+    label: "Manta PRD",
+    value: "https://config.safe.manta.network",
+  },
+  {
+    label: "Mantle PRD",
+    value: "https://config.multisig.mantle.xyz",
+  },
+  {
+    label: "Moonbeam PRD",
+    value: "https://config.multisig.moonbeam.network",
+  },
+  {
+    label: "NeonEVM PRD",
+    value: "https://config.safe.neonevm.org",
+  },
+  {
+    label: "Oasis PRD",
+    value: "https://config.safe.oasis.io",
+  },
+  {
+    label: "Reya PRD",
+    value: "https://config.safe.reya.network",
+  },
+  {
+    label: "Rootstock PRD",
+    value: "https://config.safe.rootstock.io",
+  },
+  {
+    label: "Scroll PRD",
+    value: "https://config.safe.scroll.xyz",
+  },
+  {
+    label: "Sei PRD",
+    value: "https://config.sei-safe.protofire.io",
+  },
+  {
+    label: "Shape PRD",
+    value: "https://config.safe.shape.network",
+  },
+  {
+    label: "Sophon PRD",
+    value: " https://config.safe.sophon.xyz",
+  },
+  {
+    label: "Vana Moksha Testnet PRD",
+    value: "https://config.safe.vana.org",
+  },
+  {
+    label: "Superchain PRD",
+    value: "https://config.safe.optimism.io",
+  },
+  {
+    label: "Taiko PRD",
+    value: "https://config.safe.taiko.xyz",
+  },
+  {
+    label: "Tangible PRD",
+    value: "https://config.safe.re.al",
+  },
+  {
+    label: "Telos PRD",
+    value: "https://config.safe.telos.net",
+  },
+  {
+    label: "Tenet PRD",
+    value: "https://config.safe.tenet.org",
+  },
+  {
+    label: "Wemix PRD",
+    value: "https://config.safe.wemix.com",
+  },
+  {
+    label: "ZetaChain PRD",
+    value: "https://config.safe.zetachain.com",
+  },
+  {
+    label: "Zilliqa EVM PRD",
+    value: "https://config.safe.zilliqa.com",
+  },
+  {
+    label: "zkLink PRD",
+    value: "https://config.safe.zklink.io",
+  },
+  {
+    label: "Autonomys PRD",
+    value: "https://config.safe.autonomys.xyz",
+  },
+  {
+    label: "Etherlink PRD",
+    value: "https://config.safe.etherlink.com",
+  },
+  {
+    label: "Open Campus Codex PRD",
+    value: "https://config.opencampus-codex-safe.protofire.io",
+  },
+  {
+    label: "Aleph Zero PRD",
+    value: "https://config.alephzero-safe.protofire.io",
+  },
+  {
+    label: "KAVA PRD",
+    value: "https://config.safe.kava.io",
+  },
+  {
+    label: "Morph PRD",
+    value: "https://config.safe.morphl2.io",
+  },
+  {
+    label: "Zircuit PRD",
+    value: "https://config.safe.zircuit.com",
+  },
+  {
+    label: "Saakuru PRD",
+    value: "https://config-safe.saakuru.com",
+  },
+  {
+    label: "Everclear PRD",
+    value: "https://config.safe.everclear.org",
+  },
+  {
+    label: "Xai PRD",
+    value: "https://config.safe.xai.games",
+  },
+  {
+    label: "Filecoin PRD",
+    value: "https://config.safe.filecoin.io",
+  },
+  {
+    label: "Nibiru PRD",
+    value: "https://config.safe.nibiru.fi"
+  }
+];
+
+export const defaultOptionsSTG = [
+  {
+    label: "Astar STG",
+    value: "https://config.staging-safe.astar.network",
+  },
+  {
+    label: "Berachain STG",
+    value: "https://config.staging.safe.berachain.com",
+  },
+  {
+    label: "Bob STG",
+    value: "https://config.staging.safe.gobob.xyz",
+  },
+  {
+    label: "BOBA STG",
+    value: "https://config.staging.safe.boba.network",
+  },
+  {
+    label: "CrossFi STG",
+    value: "https://config.staging.safe.crossfi.org",
+  },
+  {
+    label: "Cronos STG",
+    value: "https://config-cronos-safe.crolabs-int.co/",
+  },
+  {
+    label: "Evmos STG",
+    value: "https://config.safe.evmos.dev",
+  },
+  {
+    label: "Gelato | Reya STG",
+    value: "https://config.staging.safe.reya.network",
+  },
+  {
+    label: "Gelato | Tangible STG",
+    value: "https://config.staging.safe.re.al",
+  },
+  {
+    label: "Harmony STG",
+    value: "https://config.staging-safe.harmony.one",
+  },
+  {
+    label: "Holesky STG",
+    value: "https://config.stg.holesky-safe.protofire.io",
+  },
+  {
+    label: "Immutable STG",
+    value: "https://config.safe.staging.immutable.com",
+  },
+  {
+    label: "Flow STG",
+    value: "https://config.staging.safe.flow.com",
+  },
+  {
+    label: "IoTeX STG",
+    value: "https://config.staging.safe.iotex.io",
+  },
+  {
+    label: "Kroma STG",
+    value: "https://config.staging.safe.kroma.network",
+  },
+  {
+    label: "Linea STG",
+    value: "https://config.staging.safe.linea.build",
+  },
+  {
+    label: "Manta STG",
+    value: "https://config.staging.safe.manta.network",
+  },
+  {
+    label: "Mantle STG",
+    value: "https://config.staging.multisig.mantle.xyz",
+  },
+  {
+    label: "Moonbeam STG",
+    value: "https://config.staging.multisig.moonbeam.network",
+  },
+  {
+    label: "NeonEVM STG",
+    value: "https://config.staging.safe.neonevm.org",
+  },
+  {
+    label: "Oasis STG",
+    value: "https://config.safe.stg.oasis.io",
+  },
+  {
+    label: "Reya STG",
+    value: "https://config.staging.safe.reya.network",
+  },
+  {
+    label: "Rootstock STG",
+    value: "https://config.staging.safe.rootstock.io",
+  },
+  {
+    label: "Scroll STG",
+    value: "https://config.staging.safe.scroll.xyz",
+  },
+  {
+    label: "Sei STG",
+    value: "https://config.staging.sei-safe.protofire.io",
+  },
+  {
+    label: "Superchain STG",
+    value: "https://config.staging.safe.optimism.io",
+  },
+  {
+    label: "Taiko STG",
+    value: "https://config.staging.safe.taiko.xyz",
+  },
+  {
+    label: "Tangible STG",
+    value: "https://config.staging.safe.re.al",
+  },
+  {
+    label: "Tenet STG",
+    value: "https://config.staging.safe.tenet.org",
+  },
+  {
+    label: "Wemix STG",
+    value: "https://config.staging.safe.wemix.com",
+  },
+  {
+    label: "ZetaChain STG",
+    value: "https://config.staging.safe.zetachain.com",
+  },
+  {
+    label: "Zilliqa EVM STG",
+    value: "https://config.staging.safe.zilliqa.com",
+  },
+  {
+    label: "zkLink STG",
+    value: "https://config.staging.safe.zklink.io",
+  },
+  {
+    label: "Autonomys STG",
+    value: "https://config.staging.safe.autonomys.xyz",
+  },
+  {
+    label: "Etherlink STG",
+    value: "https://config.staging.safe.etherlink.com/",
+  },
+  {
+    label: "Open Campus Codex STG",
+    value: "https://config.staging.opencampus-codex-safe.protofire.io",
+  },
+  {
+    label: "Aleph Zero STG",
+    value: "https://config.staging.alephzero-safe.protofire.io",
+  },
+  {
+    label: "KAVA STG",
+    value: "https://config.staging.safe.kava.io",
+  },
+  {
+    label: "Morph STG",
+    value: "https://config.stg.safe.morphl2.io",
+  },
+  {
+    label: "Zircuit STG",
+    value: "https://config.staging.safe.zircuit.com",
+  },
+  {
+    label: "Saakuru STG",
+    value: "https://config-safe-stage.saakuru.com/",
+  },
+  {
+    label: "Everclear STG",
+    value: "https://config.staging.safe.everclear.org",
+  },
+  {
+    label: "Shape STG",
+    value: "https://config.staging.safe.shape.network",
+  },
+  {
+    label: "Sophon STG",
+    value: "https://config.staging.safe.sophon.xyz",
+  },
+  {
+    label: "Vana Moksha Testnet STG",
+    value: "https://config.staging.safe.vana.org",
+  },
+  {
+    label: "Xai STG",
+    value: "https://config.staging.safe.xai.games",
+  },
+  {
+    label: "Filecoin STG",
+    value: "https://config.staging.safe.filecoin.io",
+  },
+  {
+    label: "Nibiru STG",
+    value: "https://config.staging.safe.nibiru.fi"
+  }
+];
+
+export const defaultOptions = [...defaultOptionsPRD, ...defaultOptionsSTG];
